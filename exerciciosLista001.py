d0a0ce0c612eb7911b28db2f0cfde2abd48cd883
AngeloDev-New <angelo.dev.santos@gmail.com>
2024-08-21 22:17:20 -0300

    Update exerciciosLista001.py

--- exerciciosLista001.py
+++ exerciciosLista001.py
@@ -3,17 +3,25 @@ def getArea(frente,comprimento):
 
 class Padaria():
   def __init__(self):
-    lucro = int(imput("Quantos paes franceses foram vendidos?"))*0.12+int(imput("Quantas broas foram vendidas?"))*1.50
+    lucro = int(input("Quantos paes franceses foram vendidos?"))*0.12+int(input("Quantas broas foram vendidas?"))*1.50
     self.lucro = f"{round(lucro,2)} reais"
     self.poupanca = f"{round(lucro*0.1,2)} reais"
 
+class Pessoa():
+  def __init__(self):
+    self.nome = input("Qual o seu nome?").upper()
+    self.dias_vida = int(input("Quantos anos voce tem?"))*365
+
 if __name__ in "__main__":
   print("Exercicio 01")
-  print("A area do terreno tem: ",getArea(float(imput("Qual a frente do terreno?\n")),float(imput("Qual o comprimento do terreno?\n"))))
+  print("A area do terreno tem: ",getArea(float(input("Qual a frente do terreno?\n")),float(input("Qual o comprimento do terreno?\n"))))
   print("Exercicio 02")
-  print("Voce vai precisar de ",int(imput("Quantos cavalos ha no haras?:\n))*4,"ferraduras para colocar em todos os cavalos")
+  print("Voce vai precisar de ",int(input("Quantos cavalos ha no haras?:\n"))*4,"ferraduras para colocar em todos os cavalos")
   print("Exercicio 03")
   padaria = Padaria()
   print(f"O total em vendas foi {padaria.lucro}, e {padaria.poupanca} sera guardado")
+  print("Exercicio 04")
+  pessoa = Pessoa()
+  print(f"{pessoa.nome} VOCE JA VIVEU {pessoa.dias_vida} DIAS")
   
   
