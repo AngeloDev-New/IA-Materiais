b598347f9c35fb458f641bfac13ae4cb1bc89ffb
AngeloDev-New <angelo.dev.santos@gmail.com>
2024-08-21 21:40:02 -0300

    Update exerciciosLista001.py

--- exerciciosLista001.py
+++ exerciciosLista001.py
@@ -1,1 +1,11 @@
+def getArea(frente,comprimento):
+  return frente*comprimento
 
+def 
+
+if __name__ in "__main__":
+  print("Exercicio 01")
+  print("A area do terreno tem: ",getArea(float(imput("Qual a frente do terreno?\n")),float(imput("Qual o comprimento do terreno?\n"))))
+  print("Exercicio 02")
+  print("Voce vai precisar de ",int(imput("Quantos cavalos ha no haras?:\n))*4,"ferraduras para colocar em todos os cavalos")
+  
