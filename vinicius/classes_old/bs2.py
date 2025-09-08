a85078010ce1e544dbc601e130c1d1c763107ed7
AngeloDev <angelo.dev.santos@gmail.com>
2025-09-08 17:09:44 -0300

    Subindo_Xadrez

--- vinicius/classes_old/bs2.py
+++
@@ -1,8 +0,0 @@
-import requests
-url = 'https://www.google.com/'
-response = requests.get(url)
-if response.status_code == 200:
-    print("Request successful!")
-    html_content = response.text
-else:
-    print(f"Failed to retrieve content. Status code: {response.status_code}")
